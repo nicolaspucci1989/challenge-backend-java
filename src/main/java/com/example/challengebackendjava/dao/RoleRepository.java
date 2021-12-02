7424d5dee996657cd1a5a0ba5d3e4fc8dd1e711b
Nicolas Pucci <nicolaspucci1989@gmail.com>
2021-12-02 20:46:59 -0300

    agregando findBy

--- src/main/java/com/example/challengebackendjava/dao/RoleRepository.java
+++ src/main/java/com/example/challengebackendjava/dao/RoleRepository.java
@@ -2,5 +2,14 @@ package com.example.challengebackendjava.dao;
 
 import com.example.challengebackendjava.model.Role;
 
+import java.util.Objects;
+
 public class RoleRepository extends Repositorio<Role> {
+  Role findByName(String name) {
+    return elementos
+        .stream()
+        .filter(role -> Objects.equals(role.getName(), name))
+        .findFirst()
+        .orElse(null);
+  }
 }
--- src/main/java/com/example/challengebackendjava/dao/UserRepository.java
+++ src/main/java/com/example/challengebackendjava/dao/UserRepository.java
@@ -2,5 +2,13 @@ package com.example.challengebackendjava.dao;
 
 import com.example.challengebackendjava.model.User;
 
+import java.util.Objects;
+
 public class UserRepository extends Repositorio<User> {
+  User findByUsername(String username) {
+    return elementos.stream()
+        .filter(user -> Objects.equals(user.getUsername(), username))
+        .findFirst()
+        .orElse(null);
+  }
 }
