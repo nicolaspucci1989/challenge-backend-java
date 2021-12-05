2902ec389b8692d4e75e896655ab7db0577b1dc9
Nicolas Pucci <nicolaspucci1989@gmail.com>
2021-12-05 18:11:46 -0300

    custom login url

--- src/main/java/com/example/challengebackendjava/security/SecurityConfig.java
+++ src/main/java/com/example/challengebackendjava/security/SecurityConfig.java
@@ -30,11 +30,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
+    CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(authenticationManagerBean());
+    customAuthenticationFilter.setFilterProcessesUrl("/auth/login");
     http.csrf().disable();
     http.sessionManagement().sessionCreationPolicy(STATELESS);
-    http.authorizeRequests().antMatchers("/auth/refreshtoken/**").permitAll();
+    http.authorizeRequests().antMatchers("/auth/login/**","/auth/refreshtoken/**").permitAll();
     http.authorizeRequests().antMatchers("/movies/**", "/characters/**").authenticated();
-    http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
+    http.addFilter(customAuthenticationFilter);
     http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
   }
 
